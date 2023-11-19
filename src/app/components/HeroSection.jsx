6e65cfe7249c5260296d6f7deb5979476b271a9d
Tardieujose <tardieujose@gmail.com>
2023-11-19 17:03:46 -0300

    update

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -45,7 +45,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/"
+              href="https://drive.google.com/file/d/1db1yD0ga_y9n7HgbAjkE8YYUpXac3oBa/view?usp=drive_link"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-700 to-blue-700 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
 // , previewUrl
-const ProjectCard = ({ imgUrl, title, description, gitUrl }) => {
+const ProjectCard = ({ imgUrl, title, description, deployUrl }) => {
   return (
     <div>
       <div
@@ -12,7 +12,7 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl }) => {
       >
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
           <Link
-            href={gitUrl}
+            href={deployUrl}
             className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
           >
             <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
